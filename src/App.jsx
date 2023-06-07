df47b9a310b21091bcf4f8c681c08e3021409a46
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-06-07 12:47:19 +0100

    Styling and punctuation handling

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,39 @@ import { useState } from 'react'
 import './index.css'
 
 function App() {
-  const [translatedString, setTranslatedString] = useState('')
-
-
-  const translate = (sentence) =>{
-    let lowerCase = sentence.trim().toLowerCase()
-    let stringWordArray = lowerCase.split(' ')
+  const [translatedString, setTranslatedString] = useState('');
+  
+  const translate = sentence =>{
+    let lowerCase = sentence.trim().toLowerCase();
+    var punctuation = /[\\.,?!:;@#~]/g;
+    lowerCase  =lowerCase.replace(punctuation, '');
+    let stringWordArray = lowerCase.split(' ');
     for (let i = 0; i < stringWordArray.length; i++ ){
       let currentWord = stringWordArray[i]
       if(currentWord[0] === 'a' || currentWord[0] === 'e' || currentWord[0] === 'i' || currentWord[0] === 'o' || currentWord[0] === 'u'){
-        currentWord = currentWord + 'ay'
-        stringWordArray.splice(i, 1, currentWord)
+        currentWord = currentWord + 'ay';
+        stringWordArray.splice(i, 1, currentWord);
       }
       else{
-        let firstLetter = currentWord[0]
-        currentWord = currentWord.substring(1)
-        currentWord = currentWord + firstLetter + 'ay'
-        stringWordArray.splice(i, 1 ,currentWord)
+        let firstLetter = currentWord[0];
+        currentWord = currentWord.substring(1);
+        currentWord = currentWord + firstLetter + 'ay';
+        stringWordArray.splice(i, 1 ,currentWord);
       }
     }
-    setTranslatedString(stringWordArray.join(' '))
+    setTranslatedString(stringWordArray.join(' '));
   }
 
   return (
     <>
-      <div className='translation'>
-        <h1>Pig Latin Translator</h1>
-        <h2>{translatedString}</h2>
-      </div>
+      <h1>Pig Latin Translator</h1>
       
       <textarea
       placeholder='Enter your sentence'
         type='text'
         onChange={e =>translate(e.target.value)}
       />
-      
+      <h2>{translatedString}</h2>
     </>
   )
 }
